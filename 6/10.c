16e181da1185eebff4ec6b8ff79d9bddcfcb1302
personal_wow <962726743@qq.com>
2022-06-04 17:22:30 +0800

    commit

--- 6/10.c
+++ 6/10.c
@@ -7,14 +7,16 @@ int main(void)
     printf("Enter a date (mm/dd/yy): ");
     scanf("%d /%d /%d", &m1, &d1, &y1);
 
-    while (1) {
+    while (1) 
+    {
         printf("Enter a date (mm/dd/yy): ");
         scanf("%d /%d /%d", &m2, &d2, &y2);
 
         if (d2 == 0 && m2 == 0 && y2 == 0)
             break;
         if (y2 < y1 || (y1 == y2 && m2 < m1) ||
-            (y1 == y2 && m1 == m2 && d2 < d1)) {
+            (y1 == y2 && m1 == m2 && d2 < d1)) 
+        {
 
             d1 = d2;
             m1 = m2;
