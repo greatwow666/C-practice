23f184d838e8d87c4c6d2d16ff2654b58412fbe5
greatwow <962726743@qq.com>
2022-07-14 10:17:17 +0800

    commit commit

--- 8/1.c
+++ 8/1.c
@@ -1,8 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define N 10
 
 int main(void)
 {
+    bool digit_seen[N] = {false}, repeat[N] = {false};
+    long n;
+    int digit, i;
 
+    printf("Enter a number:");
+    scanf("%ld", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+        if(digit_seen[digit])
+            repeat[digit] = true;
+        else
+            digit_seen[digit] = true;
+        n = n / 10;
+    }
+    
+
+    printf("Repeated digits: ");
+    for (i = 0; i < 10 ; i++)
+    {
+        if(repeat[i])
+            printf("%d, ", i);
+        /* code */
+    }
+    
+    return 0;
 }
