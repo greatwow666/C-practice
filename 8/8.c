e08e8cf350029d18c18c3d40d13180f36b01a164
personal_wow <962726743@qq.com>
2022-07-19 22:08:24 +0800

    commit

--- 8/8.c
+++ 8/8.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-    int i, j, sum;
+    int i, j, sum, max, min;
     int student_grade[STUDENTS][CLASSES] = {0};
 
     for (i = 0; i < STUDENTS; i++)
@@ -18,7 +18,7 @@ int main(void)
         }
     }
 
-    printf("The student average is ");
+    printf("The student sums is : ");
     for (i = 0; i < STUDENTS; i++)
     {
         sum = 0;
@@ -28,6 +28,57 @@ int main(void)
             /* code */
         }
         printf("%d ", sum);
+    }
+    
+    printf("\nThe student average is : ");
+    for (i = 0; i < STUDENTS; i++)
+    {
+        sum = 0;
+        for (j = 0; j < CLASSES; j++)
+        {
+            sum = sum + student_grade[i][j];
+            /* code */
+        }
+        printf("%f ", sum/1.0/CLASSES);
+    }
+
+    printf("\nThe classes average is : ");
+    for (i = 0; i < CLASSES; i++)
+    {
+        sum = 0;
+        for (j = 0; j < STUDENTS; j++)
+        {
+            sum = sum + student_grade[j][i];
+            /* code */
+        }
+        printf("%f ", sum/1.0/CLASSES);
+    }
+
+    printf("\nThe max is : ");
+    for (i = 0; i < CLASSES; i++)
+    {
+        max = 0;
+        for (j = 0; j < STUDENTS; j++)
+        {
+            if (max < student_grade[j][i])
+            {
+                max = student_grade[j][i];
+            }
+        }
+        printf("%d ", max);
+    }
+    printf("\nThe min is : ");
+    for (i = 0; i < CLASSES; i++)
+    {
+        min = student_grade[0][i];
+        for (j = 0; j < STUDENTS; j++)
+        {
+            if (min > student_grade[j][i])
+            {
+                min = student_grade[j][i];
+            }
+        }
+        printf("%d ", min);
     }
     
     
